944686f3b4dda25fa464e317c9e27c989d46304f
Vihananand <vihananand2018@gmail.com>
2025-04-13 16:15:57 +0530

    fixed build bugs

--- app/page.js
+++ app/page.js
@@ -1,7 +1,5 @@
 "use client";
 
-import Image from "next/image";
-import Link from "next/link";
 import { useState, useEffect } from "react";
 import { getImagesByTag } from "@/utils/cloudinary";
 import Head from 'next/head';
